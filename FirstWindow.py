17b5af29a2246dc6f087bd7b86da98b18b0d651d
wEm1Xxx <sergeygevkan352006@gmail.com>
2025-04-26 11:02:33 +0500

    123

---
+++ FirstWindow.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+
+
+class FirstWindow(QMainWindow): #Создал класс FirstWindow, родители которого является QMainWindow
+    def __init__(self):# создал конструктор
+        super().__init__() #передал все атрибуты из конструктора
+        self.setWindowTitle('Первое окно')
+        self.setGeometry(100,100,400,300)
+
+        self.label = QLabel('КУ',self)
+        self.label.move(150,50) # задал кординаты label в окне
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv) #объект для создания окна
+    window = FirstWindow() #объект класса FirstWindow - Окно
+    window.show() # для объекта window применяя метод show()
+    sys.exit(app.exec_()) #из библиотеки sys используется метод
